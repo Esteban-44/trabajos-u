f0d44eb15ea0c3d09a92f6b3719d40ca6f0e6b05
Esteban <esteban04rp@gmail.com>
2026-08-16 18:28:21 -0500

    Add files via upload

---
+++ Java/ejercicios-basicos/Curso.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package curso;
+
+/**
+ *
+ * @author Janus
+ */
+public class Curso {
+     public static void drawLegs(){
+        System.out.println("     ||     ||      ");
+        System.out.println("     ||     ||      ");
+        System.out.println("    (||)   (||)     ");
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("   /\\         /\\   ");
+        System.out.println("  /  \\_______/  \\  ");
+        System.out.println(" /               \\ ");
+        System.out.println("(  /\\         /\\  )");
+        System.out.println("====     V     ====");
+        System.out.println("======(__|__)======");
+        System.out.println("  (             )  ");
+        System.out.println("   (___________)   ");
+        drawLegs();
+    }    
+
+}
+
